597e76189eb9917c77a387fa7bebd166871d7f95
Scylidose <jordisaleilles@hotmail.fr>
2023-03-07 19:56:52 -0500

    Added docstrings and remove duplicate row function.

--- src/preprocess.py
+++ src/preprocess.py
@@ -10,8 +10,37 @@ nltk.download('punkt')
 nltk.download('wordnet')
 
 
-def preprocess_text(text):
+def remove_duplicates(df, col_name):
+    """
+    Remove duplicate rows in a specific column of a Pandas DataFrame.
+    
+    Args:
+        df (Pandas DataFrame): The DataFrame to remove duplicates from.
+        col_name (str): The name of the column to remove duplicates from.
+        
+    Returns:
+        Pandas DataFrame: The DataFrame with duplicate rows removed.
+    """
+    # Create a copy of the original DataFrame
+    df_copy = df.copy()
+    
+    # Drop duplicate rows based on the specified column
+    df_copy.drop_duplicates(subset=col_name, inplace=True)
+    
+    return df_copy
 
+
+def preprocess_text(text):
+    """
+    Preprocess a text string by converting to lowercase, removing punctuation, 
+    removing stopwords, and lemmatizing the remaining words.
+    
+    Args:
+        text (str): The text string to preprocess.
+        
+    Returns:
+        str: The preprocessed text string.
+    """
     # Convert to lowercase
     text = text.lower()
 
@@ -37,6 +66,19 @@ def preprocess_text(text):
 
 
 def add_preprocessed_text_website(output_file):
+    """
+    Preprocess the 'Text' column of a CSV file containing website data.
+    The preprocessing steps include removing HTML tags, converting to lowercase,
+    removing stop words, and lemmatization. Duplicate rows based on the preprocessed text
+    are also removed to retain only unique content.
+    
+    Args:
+        output_file (str): The name of the CSV file to preprocess.
+        
+    Returns:
+        None
+    """
     df = pd.read_csv(output_file)
     df['Text'] = df['Text'].apply(preprocess_text)
+    df = remove_duplicates(df, 'Text')
     df.to_csv(output_file, index=False)
