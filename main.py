c5ff57a02aa8a61ba5d0241d6b5945b90e274913
Scylidose <jordisaleilles@hotmail.fr>
2023-03-08 19:01:45 -0500

    Added functions to extract, preprocess and make text query.

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
-from src import extract, preprocess, scraping 
+from src import extract, preprocess, scraping, index, model
 
 def main():
-    scraping.check_websites("https://nomanssky.fandom.com/", "data/websites.json")
+    website = "https://nomanssky.fandom.com/"
+    websites_file = "data/websites.json"
+    output_file = "data/links.csv"
+    db_dir = "data/whoosh"
+    child_depth = 1
+
+    print("\n-----------------------\n")
+    scraping.check_websites(website, websites_file, child_depth)
+    print("\n-----------------------\n")
+    extract.json_to_csv(websites_file, output_file)
+    print("\n-----------------------\n")
+    extract.extract_html_text(output_file)
+    print("\n-----------------------\n")
+    preprocess.add_preprocessed_text_website(output_file)
+
+    index = index.create_database(output_file, db_dir)
+
+    queryText = "What is the release date of No Man\'s sky?"
+
+    index = index.search_query(queryText, index)
 
 if __name__ == '__main__':
     main()
-
