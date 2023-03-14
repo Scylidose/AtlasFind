6b102e932bb31a8c4356ce845a2533d009df50e7
Scylidose <jordisaleilles@hotmail.fr>
2023-03-14 17:53:58 -0400

    Added Haystack, and configurations for both model, added docstring.

--- src/model.py
+++ src/model.py
@@ -1,9 +1,53 @@
 from deeppavlov import build_model, configs
 
+from haystack.document_stores import InMemoryDocumentStore
+from haystack.pipelines.standard_pipelines import TextIndexingPipeline
+from haystack.nodes import BM25Retriever, FARMReader
+from haystack.pipelines import ExtractiveQAPipeline
+from haystack.utils import print_answers
 
-model = build_model(configs.squad.squad_bert)
+import os
 
-def answer_question(documents, query):
+def configure_deeppavlov():
+    """
+    Configure a DeepPavlov Question Answering (QA) Model.
+
+    The function loads a pre-trained BERT-based model for extractive QA from the DeepPavlov library.
+
+    Returns:
+        A DeepPavlov SquadModel object that can be used to perform
+        extractive QA.
+    """
+    return build_model(configs.squad.squad_bert)
+
+def configure_haystack(doc_dir):
+    """
+    Configure a Haystack Extractive Question Answering (QA) Pipeline.
+
+    The function uses a directory containing text documents to create an in-memory document store, index the
+    documents using BM25, load a pre-trained FARM Reader model for extractive QA, and create a pipeline that combines
+    the reader and the retriever.
+
+    Args:
+        doc_dir (str): The path to the directory containing the text documents to be indexed and searched.
+
+    Returns:
+        A Haystack ExtractiveQAPipeline object that can be used to perform extractive QA.
+    """
+    document_store = InMemoryDocumentStore(use_bm25=True)
+
+    files_to_index = [doc_dir + "/" + f for f in os.listdir(doc_dir)]
+    indexing_pipeline = TextIndexingPipeline(document_store)
+    indexing_pipeline.run_batch(file_paths=files_to_index)
+
+    retriever = BM25Retriever(document_store=document_store)
+
+    reader = FARMReader(model_name_or_path="deepset/roberta-base-squad2", use_gpu=True)
+
+    return ExtractiveQAPipeline(reader, retriever)
+
+
+def deeppavlov_answer(model, documents, query):
     """
     Answer a question based on a list of documents and a query using DeepPavol model.
 
@@ -14,10 +58,6 @@ def answer_question(documents, query):
     Returns:
         The answer to the question based on the input documents.
     """
-    #document_list = []
-    #for hit in documents:
-    #    document_list.append(hit)
-    #documents = "\n\n".join(document_list)
 
     if not isinstance(documents, list):
         documents = [documents]
@@ -28,3 +68,31 @@ def answer_question(documents, query):
     answer = model(documents, query)
 
     return answer
+
+model = build_model(configs.squad.squad_bert)
+
+
+def haystack_answer(pipe, query):
+    """
+    Use a Haystack Extractive Question Answering (QA) Pipeline to answer a question.
+
+    The function takes a query (a question) and uses the Haystack pipeline to retrieve the top-k most relevant
+    documents from the in-memory document store using BM25, and then use a pre-trained FARM Reader model for extractive
+    QA to extract the answer from the retrieved documents.
+
+    Args:
+        pipe (ExtractiveQAPipeline): A Haystack ExtractiveQAPipeline object configured with a retriever and a reader.
+        query (str): The question to be answered.
+
+    Returns:
+        The extracted answer to the question.
+    """
+    prediction = pipe.run(
+        query=query,
+        params={
+            "Retriever": {"top_k": 10},
+            "Reader": {"top_k": 5}
+        }
+    )
+
+    return prediction['answers'][0].answer
