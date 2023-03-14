d1de4bca3f76ac5b7a45e06d1ccab9afbf21910b
Scylidose <jordisaleilles@hotmail.fr>
2023-03-14 17:54:05 -0400

    Added docstring.

--- src/answer.py
+++ src/answer.py
@@ -1,7 +1,23 @@
 import index, model
 
 def answer_question(model_choice, model_object, query_text, output_file, db_dir):
+    """
+    Use the specified model to answer a question based on the given query text.
 
+    Args:
+        model_choice (str): The name of the model to use for answering the question. Must be either "DeepPavlov"
+            or "Haystack".
+        model_object: An instance of the model to use. For DeepPavlov, this should be an instance of the
+            `deeppavlov.models.bertqa.BertQA` class. For Haystack, this should be an instance of the `ExtractiveQAPipeline`
+            class.
+        query_text (str): The text of the question to answer.
+        output_file (str): The path to the file where the search index should be saved (only used for DeepPavlov).
+        db_dir (str): The path to the directory where the search index should be stored (only used for DeepPavlov).
+
+    Returns:
+        The answer to the question, as determined by the specified model. If the model is unable to find an
+        answer, returns an empty string.
+    """
     if model_choice == "DeepPavlov":
         index_db = index.create_database(output_file, db_dir)
         if query_text is None:
