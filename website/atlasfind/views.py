c152fa9dd6ec2691ca20f7edbfff196f8dca62e2
Scylidose <jordisaleilles@hotmail.fr>
2023-03-14 17:55:38 -0400

    Added Haystack answer call and added more style.

--- website/atlasfind/views.py
+++ website/atlasfind/views.py
@@ -4,7 +4,14 @@ from django.http import JsonResponse
 import sys, os
 sys.path.append(os.path.abspath(os.path.join('..', 'src')))
 
-import answer
+import answer, export_doc, model
+
+documents_dir = "data/documents"
+output_file = "data/links.csv"
+db_dir = "data/whoosh"
+
+export_doc.export_documents(output_file, documents_dir)
+model_object = model.configure_haystack(documents_dir)
 
 # Create your views here.
 def home(request):
@@ -13,8 +20,9 @@ def home(request):
 def search(request):
     # retrieve the search query from the request
     search_query = request.GET.get('search_query', None)
+
     # perform search and retrieve results
-    results = answer.answer_question(search_query)
+    results = answer.answer_question("Haystack", model_object, search_query, output_file, db_dir)
 
     # prepare the data to be sent back to the client
     if not results:
