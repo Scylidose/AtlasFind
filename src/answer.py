5f88b57b49c4ea394cf0dab9e3e7f86e1657fa78
Scylidose <jordisaleilles@hotmail.fr>
2023-03-14 17:50:56 -0400

    Added model choice between deeppavlov and haystack.

--- src/answer.py
+++ src/answer.py
@@ -1,16 +1,18 @@
-import index #, model
+import index, model
 
-def answer_question(query_text):
-    output_file = "../data/links.csv"
-    db_dir = "../data/whoosh"
+def answer_question(model_choice, model_object, query_text, output_file, db_dir):
 
-    index_db = index.create_database(output_file, db_dir)
-    if query_text is None:
-        query_text = 'No Man\s sky'
-    print("---> ", query_text)
-    documents = index.search_query(query_text, index_db)
-    if len(documents) > 0:
-        return [documents[0]['title']]
-    else: 
-        return 'No Man\s Sky was created in August 2016'
-    #model.answer_question(documents[0]['content'], query_text)
+    if model_choice == "DeepPavlov":
+        index_db = index.create_database(output_file, db_dir)
+        if query_text is None:
+            query_text = ''
+
+        documents = index.search_query(query_text, index_db)
+
+        if len(documents) > 0:
+            return model.deeppavlov_answer(model_object, documents[0]['content'], query_text)
+
+    elif model_choice == "Haystack":
+        return model.haystack_answer(model_object, query_text)
+
+    return ''
