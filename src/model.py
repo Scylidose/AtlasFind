2c2192e49e94c885608db278339327804c7faf49
Scylidose <jordisaleilles@hotmail.fr>
2023-09-21 08:24:56 -0400

    Added configuration and chat function for gpt models.

--- src/model.py
+++ src/model.py
@@ -1,10 +1,6 @@
 from deeppavlov import build_model, configs
-
-from haystack.document_stores import InMemoryDocumentStore
-from haystack.pipelines.standard_pipelines import TextIndexingPipeline
-from haystack.nodes import BM25Retriever, FARMReader
-from haystack.pipelines import ExtractiveQAPipeline
-from haystack.utils import print_answers
+import openai
+from . import preprocess
 
 import os
 
@@ -19,33 +15,6 @@ def configure_deeppavlov():
         extractive QA.
     """
     return build_model(configs.squad.squad_bert)
-
-def configure_haystack(doc_dir):
-    """
-    Configure a Haystack Extractive Question Answering (QA) Pipeline.
-
-    The function uses a directory containing text documents to create an in-memory document store, index the
-    documents using BM25, load a pre-trained FARM Reader model for extractive QA, and create a pipeline that combines
-    the reader and the retriever.
-
-    Args:
-        doc_dir (str): The path to the directory containing the text documents to be indexed and searched.
-
-    Returns:
-        A Haystack ExtractiveQAPipeline object that can be used to perform extractive QA.
-    """
-    document_store = InMemoryDocumentStore(use_bm25=True)
-
-    files_to_index = [doc_dir + "/" + f for f in os.listdir(doc_dir)]
-    indexing_pipeline = TextIndexingPipeline(document_store)
-    indexing_pipeline.run_batch(file_paths=files_to_index)
-
-    retriever = BM25Retriever(document_store=document_store)
-
-    reader = FARMReader(model_name_or_path="deepset/roberta-base-squad2", use_gpu=True)
-
-    return ExtractiveQAPipeline(reader, retriever)
-
 
 def deeppavlov_answer(model, documents, query):
     """
@@ -69,30 +38,40 @@ def deeppavlov_answer(model, documents, query):
 
     return answer
 
-model = build_model(configs.squad.squad_bert)
-
-
-def haystack_answer(pipe, query):
+def configure_gpt(message, model, max_tokens):
     """
-    Use a Haystack Extractive Question Answering (QA) Pipeline to answer a question.
-
-    The function takes a query (a question) and uses the Haystack pipeline to retrieve the top-k most relevant
-    documents from the in-memory document store using BM25, and then use a pre-trained FARM Reader model for extractive
-    QA to extract the answer from the retrieved documents.
+    Configure a GPT-based language model for generating responses to a message.
 
     Args:
-        pipe (ExtractiveQAPipeline): A Haystack ExtractiveQAPipeline object configured with a retriever and a reader.
-        query (str): The question to be answered.
+        message (str): The input message or prompt.
+        model (dict): A dictionary representing the GPT model, including its properties.
+        max_tokens (int): The maximum number of tokens allowed in the generated response.
 
     Returns:
-        The extracted answer to the question.
+        str: A truncated input message suitable for GPT-based model generation.
     """
-    prediction = pipe.run(
-        query=query,
-        params={
-            "Retriever": {"top_k": 10},
-            "Reader": {"top_k": 5}
-        }
+    safety_margin = int(model['token_length']*0.25)
+    truncated_prompt, word_count = preprocess.truncate_text(message, model['token_length'] - max_tokens - safety_margin)
+    
+    return truncated_prompt
+
+def chat_with_gpt(message, model):
+    """
+    Generate a response using GPT-based chat capabilities.
+
+    Args:
+        message (list): A list of message objects with 'role' and 'content'.
+        model (dict): A dictionary containing the GPT-based chat model information.
+
+    Returns:
+        str: A generated response based on the input messages.
+    """
+    response = openai.ChatCompletion.create(
+        model=model['name'],
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": message}
+                ]
     )
 
-    return prediction['answers'][0].answer
+    return response.choices[0].message.content
