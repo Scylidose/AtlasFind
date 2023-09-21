de39fece4f049c744ce0ce720ff3e7d614cd926c
Scylidose <jordisaleilles@hotmail.fr>
2023-09-21 08:25:41 -0400

    Added customized prompt and model answering for gpt models.

--- src/answer.py
+++ src/answer.py
@@ -1,4 +1,21 @@
-import index, model
+from . import index, model
+
+models = {
+    "gpt-3.5-4k-tokens": {
+        "name": "gpt-3.5-turbo", 
+        "token_length": 4096,
+        "input_cost": 0.0015,
+        "output_cost": 0.002,
+        "available": True 
+    },
+    "gpt-3.5-16k-tokens": {
+        "name": "gpt-3.5-turbo-16k", 
+        "token_length": 16384,
+        "input_cost": 0.003,
+        "output_cost": 0.004,
+        "available": True 
+    }
+}
 
 def answer_question(model_choice, model_object, query_text, output_file, db_dir):
     """
@@ -6,10 +23,9 @@ def answer_question(model_choice, model_object, query_text, output_file, db_dir)
 
     Args:
         model_choice (str): The name of the model to use for answering the question. Must be either "DeepPavlov"
-            or "Haystack".
+            or "ChatHPT".
         model_object: An instance of the model to use. For DeepPavlov, this should be an instance of the
-            `deeppavlov.models.bertqa.BertQA` class. For Haystack, this should be an instance of the `ExtractiveQAPipeline`
-            class.
+            `deeppavlov.models.bertqa.BertQA` class.
         query_text (str): The text of the question to answer.
         output_file (str): The path to the file where the search index should be saved (only used for DeepPavlov).
         db_dir (str): The path to the directory where the search index should be stored (only used for DeepPavlov).
@@ -18,17 +34,25 @@ def answer_question(model_choice, model_object, query_text, output_file, db_dir)
         The answer to the question, as determined by the specified model. If the model is unable to find an
         answer, returns an empty string.
     """
+    
+    index_db = index.create_database(output_file, db_dir)
+    if query_text is None:
+        query_text = ''
+
+    documents = index.search_query(query_text, index_db)
+
     if model_choice == "DeepPavlov":
-        index_db = index.create_database(output_file, db_dir)
-        if query_text is None:
-            query_text = ''
-
-        documents = index.search_query(query_text, index_db)
-
         if len(documents) > 0:
-            return model.deeppavlov_answer(model_object, documents[0]['content'], query_text)
+            return model.deeppavlov_answer(model_object, body, query_text)
+    else:
+        body = ' '.join(item['content'] for item in documents)
+        negResponse = "I'm unable to answer the question based on the information I have."
 
-    elif model_choice == "Haystack":
-        return model.haystack_answer(model_object, query_text)
+        prompt = f"Answer this question: {query_text}\nUsing only the information from this documentation: {body}\nIf the answer is not contained in the supplied doc reply '{negResponse}' and nothing else"
+        
+        truncated_prompt = model.configure_gpt(prompt, models[model_choice], 1024)
+
+        if model_choice.startswith("gpt"):
+            return model.chat_with_gpt(truncated_prompt, models[model_choice])
 
     return ''
